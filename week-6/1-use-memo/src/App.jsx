cf8db9a9a4319b0e8483ea7bb87faee736c2b3e0
SugamChaudharry <nextgenachieverss@gmail.com>
2024-08-21 07:54:00 +0530

    use-memo done

--- week-6/1-use-memo/src/App.jsx
+++ week-6/1-use-memo/src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { Assignment1 } from './components/Assignment1'
 import { Assignment2 } from './components/Assignment2'
 import { Assignment3 } from './components/Assignment3'
@@ -6,9 +5,9 @@ import { Assignment3 } from './components/Assignment3'
 function App() {
   return (
     <>
-      <Assignment1 />
+      {/* <Assignment1 /> */}
       {/* <Assignment2 /> */}
-      {/* <Assignment3 /> */}
+      <Assignment3 />
     </>
   )
 }
